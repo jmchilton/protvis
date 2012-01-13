295ba2edcef18fc39cddeb4a08a8ba5ddec2fc72
Andrew Brock <a.brock@latrobe.edu.au>
2012-01-13 16:03:31 +1100

    Routing of wires between nodes in the same line is more intelegent

--- res/scripts/flowchart.js
+++ res/scripts/flowchart.js
@@ -1,71 +1,195 @@
 dojo.require("dojox.gfx");
-var Surface, Group;
-var Matrix = dojox.gfx.matrix, InitialMatrix = Matrix.translate(0, 0);
-var CanvasW, CanvasH;
+
+//Constants
+var BoxSlotSpacing = 7;
+var BoxSlots = 3;
 var BoxWidth = 140;
 var BoxHeight = 45;
-var BoxHSpacing = 50, BoxVSpacing = 30, BoxVSize = BoxVSpacing + BoxHeight;
+var BoxHSpacing = 50, BoxVSpacing = (BoxSlots * 2 + 1) * BoxSlotSpacing, BoxVSize = BoxVSpacing + BoxHeight;
 var Colors = [ "#ff5900", "#0d56ff", "#4dde00", "#a600a6", "#ff9700", "#2618b1", "#1049a9", "#009999", "#e9003a" ];
-var Nodes = new Array();
-var Selected = -1;
 
-function UpdateMatrix(){
-	group.setTransform([m.scaleAt(scaling, CanvasW/2, CanvasH/2), InitialMatrix]);
-}
-
-function FlowChartCreate(files, OnSelect) {
-	function Lerp(a, b, f) {
-		return (a * (1 - f)) + (b * f);
-	}
-
+FlowChart = function(files, OnSelect) {
+	this.Surface = null;
+	this.Matrix = dojox.gfx.matrix;
+	this.Nodes = new Array();
+	this.Selected = -1;
+	
+	var columns = new Array();
+	var obj = this;
+	
 	function MakeBox(file, selected) {
 		var color = Colors[file["index"] % Colors.length];//file["missing"] ? "gray" : "black";
-		var mat = Matrix.translate(file["x"], file["y"]);
-		var g = Surface.createGroup().setTransform(mat);
+		var mat = obj.Matrix.translate(file["x"], file["y"]);
+		var g = obj.Surface.createGroup().setTransform(mat);
 		var box = g.createRect({x:-BoxWidth/2, y:-BoxHeight/2, width:BoxWidth, height:BoxHeight, r:5}).setFill({type:"linear", y1:-BoxHeight * 0.75, x2:0, y2:BoxHeight, colors:[{offset:0, color:"white"}, {offset:1, color:color}]}).setStroke({color:"black", width:2});
 		var title = g.createText({x:0, y:-3, text:file["name"], align:"middle"}).setFont({family:"Arial", size:"14px", weight:"bold"}).setFill("black");
 		var type = g.createText({x:0, y:14, text:GetTypeName(file["type"], true), align:"middle"}).setFont({family:"Arial", size:"12px"}).setFill("black");
-		var mover = null;//new dojox.gfx.Moveable(g);
-		g.connect("onmousedown", this,function(evt) { OnSelect(evt, file); });
+		g.connect("onmousedown", this,function(evt) { if (OnSelect) { OnSelect(evt, file); } });
 		g.rawNode.setAttribute("cursor", "pointer");
-		return { box:box, title:title, type:type, g:g, mat:mat, mover:mover, connections:[] };
+		return { box:box, title:title, type:type, g:g, mat:mat, slots_top:[], slots_bot:[], x:file["x"], y:file["y"] };
+	}
+	
+	function BoxFromPosition(col, y) {
+		col = columns[col]
+		for (var b in col) {
+			if (files[col[b]]["y"] == file1["y"]) {
+				return files[col[b]];
+			}
+		}
+		return null;
 	}
 
-	function MakeConnection(file1, file2) {
-		var stroke = {color:Colors[file1["index"] % Colors.length], width:2};//(file1["missing"] || file2["missing"]) ? "gray" : "black";
-		if (file2["col"] == file1["col"] + 1) {
-			if (file2["y"] == file1["y"]) {
-				Surface.createLine({x1:file1["x"] + BoxWidth/2, y1:file1["y"], x2:file2["x"] - BoxWidth/2, y2:file2["y"]}).setStroke(stroke);
-			} else {
-				var x1 = file1["x"] + BoxWidth / 2;
-				var x2 = file2["x"] - BoxWidth / 2;
-				var path = Surface.createPath().setStroke(stroke);
-				path.moveTo(x1, file1["y"]);
-				path.curveTo(Lerp(x1, x2, 0.8), file1["y"], Lerp(x2, x1, 0.8), file2["y"], x2, file2["y"]);
+	function ConnectBoxes(files) {
+		var deffered = new Array();
+		
+		function Lerp(a, b, f) {
+			return (a * (1 - f)) + (b * f);
+		}
+		
+		function CountConnections(side) {
+			var len = 0;
+			for (s in side) {
+				len += side[s];
 			}
-		} else {
-			//FIXME: this won't work for everything
-			if (file2["y"] == file1["y"]) {
-				var x1 = file1["x"] + BoxWidth / 2;
-				var x2 = x1 + BoxHSpacing;
-				var x4 = file2["x"] - BoxWidth / 2;
-				var x3 = x4 - BoxHSpacing;
-				var y1 = file1["y"];
-				var y2 = y1 + BoxHeight / 2 + 5;
-				Surface.createLine({x1:x2, y1:y2, x2:x3, y2:y2}).setStroke(stroke);
-				var path = Surface.createPath().setStroke(stroke);
-				path.moveTo(x1, y1);
-				path.curveTo(Lerp(x1, x2, 0.8), y1, Lerp(x2, x1, 0.8), y2, x2, y2);
-				path.moveTo(x3, y2);
-				path.curveTo(Lerp(x3, x4, 0.8), y2, Lerp(x4, x3, 0.8), y1, x4, y1);
-			} else {
-				var x1 = file2["x"] - BoxWidth / 2 - BoxHSpacing;
-				var x2 = file2["x"] - BoxWidth / 2;
-				Surface.createLine({x1:file1["x"] + BoxWidth/2, y1:file1["y"], x2:x1, y2:file1["y"]}).setStroke(stroke);
-				var path = Surface.createPath().setStroke(stroke);
-				path.moveTo(x1, file1["y"]);
-				path.curveTo(Lerp(x1, x2, 0.8), file1["y"], Lerp(x2, x1, 0.8), file2["y"], x2, file2["y"]);
+			return len;
+		}
+		
+		function NextSlot(box, side) {
+			if (box[side].length < BoxSlots) {
+				return box[side].push(1);
 			}
+			var minval = 65535;
+			var minpos = 0;
+			for (s in box[side]) {
+				var c = box[side][s];
+				if (c < minval) {
+					minval = c;
+					minpos = s;
+				}
+			}
+			return minpos;
+		}
+		
+		function RouteConnectionInline(file1, file2, stroke) {
+			//find a list of all the bokes we must route past
+			var boxes = new Array();
+			var y = file1["y"];
+			for (var col = file1["col"] + 1; col < file2["col"]; ++col) {
+				var box = null;
+				for (var c in columns[col]) {
+					if (files[columns[col][c]]["y"] == y) {
+						box = obj.Nodes[columns[col][c]];
+						break;
+					}
+				}
+				boxes.push(box);
+			}
+			//find the path which has the most space and least crossovers
+			var fulltop = 0, maxtop = 0;
+			var fullbot = 0, maxbot = 0;
+			for (var b in boxes) {
+				if (boxes[b] != null) {
+					maxtop = Math.max(maxtop, CountConnections(boxes[b]["slots_top"]));
+					maxbot = Math.max(maxbot, CountConnections(boxes[b]["slots_bot"]));
+					if (boxes[b]["slots_top"].length >= BoxSlots) {
+						++fulltop;
+					}
+					if (boxes[b]["slots_bot"].length >= BoxSlots) {
+						++fullbot;
+					}
+				}
+			}
+			var side = fulltop < fullbot || (fulltop == fullbot && maxtop <= maxbot) ? -1 : 1;
+			var slot = side < 0 ? "slots_top" : "slots_bot";
+			var path = obj.Surface.createPath().setStroke(stroke);
+			var x1 = file1["x"] + BoxWidth / 2;
+			var y1 = y;
+			var b, x2, y2;
+			path.moveTo(x1, y1);
+			for (var i in boxes) {
+				b = boxes[i];
+				x2 = b["x"] - BoxWidth / 2;
+				y2 = y + side * (BoxHeight / 2 + NextSlot(b, slot) * BoxSlotSpacing);
+				path.curveTo(Lerp(x1, x2, 0.8), y1, Lerp(x2, x1, 0.8), y2, x2, y2); //draw the curved start
+				path.curveTo(x2 + BoxWidth, y2, x2, y2, x2 + BoxWidth, y2); //draw the straight line segment
+				x1 = x2 + BoxWidth;
+				y1 = y2;
+			}
+			x2 = file2["x"] - BoxWidth / 2;
+			y2 = y;
+			path.curveTo(Lerp(x1, x2, 0.8), y1, Lerp(x2, x1, 0.8), y, x2, y);
+		}
+		
+		function RouteConnection(file1, file2, stroke) {
+			var x1 = file2["x"] - BoxWidth / 2 - BoxHSpacing;
+			var x2 = file2["x"] - BoxWidth / 2;
+			obj.Surface.createLine({x1:file1["x"] + BoxWidth/2, y1:file1["y"], x2:x1, y2:file1["y"]}).setStroke(stroke);
+			var path = obj.Surface.createPath().setStroke(stroke);
+			path.moveTo(x1, file1["y"]);
+			path.curveTo(Lerp(x1, x2, 0.8), file1["y"], Lerp(x2, x1, 0.8), file2["y"], x2, file2["y"]);
+		}
+		
+		function MakeConnection(file1, file2) {
+			var stroke = {color:Colors[file1["index"] % Colors.length], width:2};//(file1["missing"] || file2["missing"]) ? "gray" : "black";
+			if (file2["col"] == file1["col"] + 1) {
+				if (file2["y"] == file1["y"]) {
+					obj.Surface.createLine({x1:file1["x"] + BoxWidth/2, y1:file1["y"], x2:file2["x"] - BoxWidth/2, y2:file2["y"]}).setStroke(stroke);
+				} else {
+					var x1 = file1["x"] + BoxWidth / 2;
+					var x2 = file2["x"] - BoxWidth / 2;
+					var path = obj.Surface.createPath().setStroke(stroke);
+					path.moveTo(x1, file1["y"]);
+					path.curveTo(Lerp(x1, x2, 0.8), file1["y"], Lerp(x2, x1, 0.8), file2["y"], x2, file2["y"]);
+				}
+			} else {
+				//FIXME: this won't work for everything
+				if (file2["y"] == file1["y"]) {
+					var direct = true;
+					for (var col = file1["col"] + 1; col < file2["col"]; ++col) {
+						for (var c in columns[col]) {
+							if (files[columns[col][c]]["y"] == file1["y"]) {
+								direct = false;
+								break;
+							}
+						}
+					}
+					if (direct) {
+						//This is an empty cell, we can do a straight line across it
+						obj.Surface.createLine({x1:file1["x"] + BoxWidth/2, y1:file1["y"], x2:file2["x"] - BoxWidth/2, y2:file2["y"]}).setStroke(stroke);
+					} else {
+						if (file2["col"] - file1["col"] == 2) {
+							RouteConnectionInline(file1, file2, stroke);
+						} else {
+							deffered.push([file1, file2, stroke]);
+						}
+						
+					}
+				} else {
+					deffered.push([file1, file2, stroke]);
+				}
+			}
+		}
+
+		for (var f in files) {
+			var deps = files[f]["deps"]
+			for (var d in deps) {
+				c = MakeConnection(files[f], files[deps[d]]);
+			}
+		}
+		var jumplen = 2;
+		while (deffered.length > 0) {
+			for (var i = deffered.length - 1; i >= 0; --i) {
+				d = deffered[i];
+				if (d[1]["col"] - d[0]["col"] == jumplen) {
+					if (d[0]["y"] == d[1]["y"]) {
+						RouteConnectionInline(d[0], d[1], d[2]);
+					} else {
+						RouteConnection(d[0], d[1], d[2]);
+					}
+					deffered.splice(i, 1);
+				}
+			}
+			++jumplen;
 		}
 	}
 
@@ -132,11 +256,24 @@ function FlowChartCreate(files, OnSelect) {
 		}
 		return 1;
 	}
+
+	this.Select = function(index) {
+		if (this.Selected >= 0) {
+			this.Nodes[this.Selected].box.setStroke({color:"black", width:2});
+			this.Nodes[this.Selected].title.setFill("black");
+			this.Nodes[this.Selected].type.setFill("black");
+		}
+		if (index >= 0) {
+			this.Nodes[index].box.setStroke({color:"white", width:2});
+			this.Nodes[index].title.setFill("white");
+			this.Nodes[index].type.setFill("white");
+		}
+		this.Selected = index;
+	}
 	
 	var x = BoxWidth / 2 + 10;
 	//seperate into columns of dependencies
 	var can_depend = new Array();
-	var columns = new Array();
 	var remaining = files.slice(0);
 	var indices = new Array();
 	for (var i in files) {
@@ -170,29 +307,9 @@ function FlowChartCreate(files, OnSelect) {
 	height = CalcGroupHeight(height);
 	OffsetChain(files, columns, columns[0][0], 0, height / 2);
 
-	Surface = dojox.gfx.createSurface(dojo.byId("navigation_graph"), 10 + BoxHSpacing * (columns.length - 1) + BoxWidth * columns.length + 10, 10 + height + 10);
+	this.Surface = dojox.gfx.createSurface(dojo.byId("navigation_graph"), 10 + BoxHSpacing * (columns.length - 1) + BoxWidth * columns.length + 10, height + 6 + BoxSlots * BoxSlotSpacing);
 	for (var f in files) {
-		Nodes.push(MakeBox(files[f]));
+		this.Nodes.push(MakeBox(files[f]));
 	}
-	for (var f in files) {
-		var deps = files[f]["deps"]
-		for (var d in deps) {
-			c = MakeConnection(files[f], files[deps[d]]);
-			//boxes[f]["connections"].push({
-		}
-	}
-}
-
-function FlowChartSelect(index) {
-	if (Selected >= 0) {
-		Nodes[Selected].box.setStroke({color:"black", width:2});
-		Nodes[Selected].title.setFill("black");
-		Nodes[Selected].type.setFill("black");
-	}
-	if (index >= 0) {
-		Nodes[index].box.setStroke({color:"white", width:2});
-		Nodes[index].title.setFill("white");
-		Nodes[index].type.setFill("white");
-	}
-	Selected = index;
+	ConnectBoxes(files);
 }
