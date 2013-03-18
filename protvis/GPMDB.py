3d560b559b9280e990cbbad5069faf721241f0b7
John Chilton <jmchilton@gmail.com>
2013-03-17 22:57:04 -0500

    PEP-8 fixes for GPMDB

--- protvis/GPMDB.py
+++ protvis/GPMDB.py
@@ -2,54 +2,63 @@ import json
 import urllib2
 import urllib
 from pyramid.response import Response
+from Common import HTTPBadRequest_Param
+from pyramid.httpexceptions import HTTPNotFound
 
-def uniprot_map_protein_id(from_type,to_type,query_id):
+
+def uniprot_map_protein_id(from_type, to_type, protein_id):
     mapping_url = 'http://www.uniprot.org/mapping/'
     params = {
-    'from':'ID',
-    'to':'ENSEMBL_PRO_ID',
-    'format':'list',
-    'query': protein_id
+    'from': 'ID',
+    'to': 'ENSEMBL_PRO_ID',
+    'format': 'list',
+    'query': protein_id,
     }
-
 
     data = urllib.urlencode(params)
     request = urllib2.Request(mapping_url, data)
     response = urllib2.urlopen(request)
     response_string = response.read(200000)
-    id_list=str(response_string).split()
+    id_list = str(response_string).split()
+    return id_list
+
 
 def proteins_for_peptide_sequence(seq):
     urlrequest = urllib2.urlopen("http://gpmdb.thegpm.org/1/peptide/accessions/seq=%s" % seq)
     return json.loads(urlrequest.read())
 
+
 def total_peptide_count(pep_seq):
-    urlrequest=urllib2.urlopen("http://gpmdb.thegpm.org/1/peptide/count/seq=%s" % pep_seq)
+    urlrequest = urllib2.urlopen("http://gpmdb.thegpm.org/1/peptide/count/seq=%s" % pep_seq)
     return json.loads(urlrequest.read())
 
-def peptide_counts_for_protein(prot_acc,pep_seq):
-    urlrequest= urllib2.urlopen("http://gpmdb.thegpm.org/1/protein/peptide_count/acc=%s&seq=%s" % (prot_acc,pep_seq))
+
+def peptide_counts_for_protein(prot_acc, pep_seq):
+    urlrequest = urllib2.urlopen("http://gpmdb.thegpm.org/1/protein/peptide_count/acc=%s&seq=%s" % (prot_acc, pep_seq))
     return json.loads(urlrequest.read())
+
 
 def total_peptide_counts_for_protein(prot_acc):
-    urlrequest=urllib2.urlopen("http://gpmdb.thegpm.org/1/protein/peptides_z/acc=%s" % prot_acc)
+    urlrequest = urllib2.urlopen("http://gpmdb.thegpm.org/1/protein/peptides_z/acc=%s" % prot_acc)
     return json.loads(urlrequest.read())
 
+
 def info_for_protein(prot_acc):
-    urlrequest= urllib2.urlopen("http://gpmdb.thegpm.org/1/protein/description/acc=%s" % prot_acc)
+    urlrequest = urllib2.urlopen("http://gpmdb.thegpm.org/1/protein/description/acc=%s" % prot_acc)
     return json.loads(urlrequest.read())
+
 
 def detailed_counts_for_peptide(peptide_seq):
     # Get all protein accessions for a peptide
     # Then go through for each protein and get the relative frequency with which that peptide
-    prots=proteins_for_peptide_sequence(peptide_seq)
+    prots = proteins_for_peptide_sequence(peptide_seq)
 
-    output={}
+    output = {}
     for protein in prots.keys():
-        counts=peptide_counts_for_protein(str(protein),peptide_seq)
-        totals=total_peptide_counts_for_protein(str(protein))
-        info=info_for_protein(str(protein))
-        output[str(protein)]={'counts':counts,'info':info,'accession':str(protein),'totals':totals}
+        counts = peptide_counts_for_protein(str(protein), peptide_seq)
+        totals = total_peptide_counts_for_protein(str(protein))
+        info = info_for_protein(str(protein))
+        output[str(protein)] = {'counts': counts, 'info': info, 'accession': str(protein), 'totals': totals}
 
     return output
 
@@ -63,8 +72,8 @@ def GetObservationsForPeptide(req):
     except:
         return HTTPBadRequest_Param("peptideseq")
 
-    pep_count=total_peptide_count(peptide_seq)
-    if len(pep_count)==0:
+    pep_count = total_peptide_count(peptide_seq)
+    if len(pep_count) == 0:
         return HTTPNotFound()
 
     return Response(str(pep_count[0]))
